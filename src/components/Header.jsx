f918997a33e8bc60e7dfce05374443c4c066ba30
Kriiscor <Corentindubail@gmail.com>
2024-04-08 14:48:29 +0200

    Nettoyage du code + activité 4

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+const Header = () => {
+    return ( 
+        <header> 
+        <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Mexicain avec une guitare" /> 
+            <nav>
+                <li><a href="/">"Acceuil"</a></li>
+            </nav>
+        </header>
+    );
+}
+export default Header;
