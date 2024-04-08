78509f314a1083cb2bf035332729032f9ac8da17
Kriiscor <Corentindubail@gmail.com>
2024-04-08 14:56:55 +0200

    modification de l'indentation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react"
 const Header = () => {
     return ( 
         <header> 
-        <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Mexicain avec une guitare" /> 
+            <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Mexicain avec une guitare" /> 
             <nav>
                 <li><a href="/">"Acceuil"</a></li>
             </nav>
