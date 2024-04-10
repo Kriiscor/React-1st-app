82cf4cac9388498a2d219df1e46e28f1fb8fe927
Kriiscor <Corentindubail@gmail.com>
2024-04-10 12:15:11 +0200

    création du tableau dishes utilisation de map() et key

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,33 +6,45 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 function Home() {
+  const dishes = [
+    {
+      id: 1,
+      name: "Tacos à l’unité",
+      price: "3€",
+      picture:
+        "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+      slug: "tacos-a-l-unite",
+    },
+    {
+      id: 2,
+      name: "Enchiladas",
+      price: "12€",
+      picture:
+        "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+      slug: "enchiladas",
+    },
+    {
+      id: 3,
+      name: "Mole poblano",
+      price: "15€",
+      picture:
+        "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+      slug: "mole-poblano",
+    },
+  ];
   return (
     <Container>
       <Row>
-        <Col>
-          <Dish
-            name="Tacos à l’unité"
-            price="3€"
-            picture="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-            slug="tacos-a-l-unite"
-          />
-        </Col>
-        <Col>
-          <Dish
-            name="Enchiladas"
-            price="12€"
-            picture="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-            slug="enchiladas"
-          />
-        </Col>
-        <Col>
-          <Dish
-            name="Mole poblano"
-            price="15€"
-            picture="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-            slug="mole-poblano"
-          />
-        </Col>
+        {dishes.map((dish) => (
+          <Col key={dish.id}>
+            <Dish
+              name={dish.name}
+              price={dish.price}
+              picture={dish.picture}
+              slug={dish.slug}
+            />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
