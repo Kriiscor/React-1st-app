7e25e96e189007a44cc99917b8a7f78ff66dac2f
Kriiscor <Corentindubail@gmail.com>
2024-04-10 11:34:18 +0200

    Changement de la classe du logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header>
       <Navbar className="bg-body-tertiary">
         <Container>
-          <NavLink to="/" className={"nav-link"}>
+          <NavLink to="/" className={"nav-brand"}>
             <img
               src={Logo}
               alt="Mexicain avec une guitare"
