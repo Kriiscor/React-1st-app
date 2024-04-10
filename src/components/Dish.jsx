4046312fe0564ac62c5c2bb0dfda2136fe59822e
Kriiscor <Corentindubail@gmail.com>
2024-04-10 15:00:18 +0200

    Ajout du badge Nouveau sur les plats

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import "../assets/styles/dish.css";
 import { NavLink } from "react-router-dom";
-const Dish = ({ name, price, picture , slug}) => {
+import { Badge } from "react-bootstrap";
+const Dish = ({ name, price, picture, slug, brandnew }) => {
   return (
     <NavLink to={`/plat/${slug}`}>
       <Card>
         <Card.Img variant="top" src={picture} />
+        <Card.ImgOverlay >
+          {brandnew === true && <Badge className="position-absolute end-0 translate-middle-x" bg="primary">Nouveau</Badge>}
+        </Card.ImgOverlay>
         <Card.Body>
           <Card.Text>{name}</Card.Text>
           <Card.Text>{price}</Card.Text>
