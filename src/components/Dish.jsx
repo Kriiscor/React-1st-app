640ea8f6cdc543b34cb44af14d6c8975dd0873b5
Kriiscor <Corentindubail@gmail.com>
2024-04-10 15:29:26 +0200

    légère modification du dish.jsx, suppréssion des commentaires

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -3,13 +3,13 @@ import Card from "react-bootstrap/Card";
 import "../assets/styles/dish.css";
 import { NavLink } from "react-router-dom";
 import { Badge } from "react-bootstrap";
-const Dish = ({ name, price, picture, slug, brandnew }) => {
+const Dish = ({ name, price, picture, slug, isNew }) => {
   return (
     <NavLink to={`/plat/${slug}`}>
       <Card>
         <Card.Img variant="top" src={picture} />
         <Card.ImgOverlay >
-          {brandnew === true && <Badge className="position-absolute end-0 translate-middle-x" bg="primary">Nouveau</Badge>}
+          {isNew  && <Badge className="position-absolute end-0 translate-middle-x" bg="primary">Nouveau</Badge>}
         </Card.ImgOverlay>
         <Card.Body>
           <Card.Text>{name}</Card.Text>
@@ -17,11 +17,6 @@ const Dish = ({ name, price, picture, slug, brandnew }) => {
         </Card.Body>
       </Card>
     </NavLink>
-    // <div className="dish">
-    //   <h2>{Name}</h2>
-    //   <img src={Picture} alt="" />
-    //   <h3>{Price}</h3>
-    // </div>
   );
 };
 export default Dish;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
       picture:
         "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
       slug: "tacos-a-l-unite",
-      brandnew : true ,
+      isNew : true ,
     },
     {
       id: 2,
@@ -23,7 +23,7 @@ function Home() {
       picture:
         "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
       slug: "enchiladas",
-      brandnew : false,
+      isNew : false,
     },
     {
       id: 3,
@@ -32,7 +32,7 @@ function Home() {
       picture:
         "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
       slug: "mole-poblano",
-      brandnew : false,
+      isNew : false,
     },
   ];
   return (
@@ -45,7 +45,7 @@ function Home() {
               price={dish.price}
               picture={dish.picture}
               slug={dish.slug}
-              brandnew={dish.brandnew}
+              isNew={dish.isNew}
             />
           </Col>
         ))}
