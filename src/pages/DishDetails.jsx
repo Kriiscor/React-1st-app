9335fa1e03134a299938e637cd43411ad3ecc4df
Kriiscor <Corentindubail@gmail.com>
2024-04-11 10:11:25 +0200

    Rendre la page des plats fonctionnelle

--- src/pages/DishDetails.jsx
+++ src/pages/DishDetails.jsx
@@ -3,17 +3,49 @@ import { useParams } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import dishesData from "../datas/dishes.json";
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 
-function DishDetails () {
+function DishDetails() {
   const { slug } = useParams();
+  const [dish, setDish] = useState(null);
+  useEffect(() => {
+    const selectedDish = dishesData.find((dish) => dish.slug === slug);
+    if (selectedDish) {
+      setDish(selectedDish);
+    }
+  }, [slug]);
+  if (!dish) {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h1>Le plat demandé n'existe pas.</h1>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
   return (
     <Container>
       <Row>
         <Col>
-          <h1>Détails du plat avec le slug :{slug}</h1>
+          <Row>
+            <Col>
+              <img src={dish.picture} alt={dish.slug} />
+            </Col>
+            <Col>
+              <h2>{dish.name}</h2>
+              <p>{dish.description}</p>
+              <p>{dish.price}€</p>
+              <Button variant="primary">Commander</Button>
+            </Col>
+            <Col></Col>
+          </Row>
         </Col>
       </Row>
     </Container>
   );
-};
+}
 export default DishDetails;
